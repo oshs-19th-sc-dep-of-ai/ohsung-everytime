4ab0e327ae9a7be99fac7c6edcd45f573ef60c4b
Shindaeyoon <shinduck52@gmail.com>
2026-01-22 05:11:50 +0900

    급식 블루프린트 연결

--- backend/app.py
+++ backend/app.py
@@ -7,8 +7,10 @@ from src.utils.config_util import ConfigManager as Config
 
 from src.routes.auth import auth_bp
 from src.routes.comments import comments_bp
+from src.routes.meal import meal_bp
 
 app = Flask(__name__)
+app.json.sort_keys = False
 
 # 프론트엔드와 세션 유지 가능하게 설정 (프론트엔드에서 꼭 withCredentials: true 확인!)
 ALLOWED_ORIGINS = ["http://localhost:5173"]
@@ -25,6 +27,7 @@ app.config['SESSION_KEY_PREFIX'] = Config().get()["Session"]["KeyPrefix"]
 # ★ 로컬 개발환경에서 쿠키 전달 보장 (CORS + 다른 포트 사용 시 필수)
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'   # 프론트/백엔드 포트가 달라도 쿠키 전달
 app.config['SESSION_COOKIE_SECURE']   = False   # http 환경에서는 False (https에서만 True)
+app.config["JSON_SORT_KEYS"] = False
 
 Session(app)
 
@@ -38,6 +41,7 @@ DatabaseManager().connect(
 # 블루프린트 등록
 app.register_blueprint(auth_bp)           # 로그인 & 로그아웃
 app.register_blueprint(comments_bp, url_prefix='/api') # 댓글 (prefix 추가)
+app.register_blueprint(meal_bp)
 
 if __name__ == '__main__':
     try:
